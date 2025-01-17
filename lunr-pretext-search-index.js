577ba0ad6ef029d7c7c94e9a76c0abc7e63a45bf
suohyd <86884865+suohyd@users.noreply.github.com>
2025-01-17 11:22:45 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -178,7 +178,7 @@ var ptx_lunr_docs = [
   "type": "Section",
   "number": "1.3",
   "title": "Subspace topology",
-  "body": " Subspace topology We begin with the following proposition.   Let a topological space and let be a subset of . Following topologies defined on are all equivalent.    The topology .    The coarsest topology on for which the canonical inclusion is continuous.    (Universal property of subspace topology). The topology on satisfying the following property.  For every topological space and any function , is continuous if and only if is continuous. See the following diagram.   The universal property of the subspace topology           The equivalence of topologies and is left as an exercise.  We show that . Consider the following diagram, where is the identity map.      Note that , and with the topology the inclusion is continuous. By the universal property of , the identity is also continuous. Hence, .  We now consider the following.      As is continuous, using the universal property of , we get that is also continuous. Since the is the coarsest topology that makes inclusion continuous, we have that .     Let be a topological space, and let be a subset of . The subspace topology on is any of the topologies defined by .    Suppose that . We can consider a product topology on as well as subspace topology induced by a product topology on . Are these topologies same on ?  "
+  "body": " Subspace topology We begin with the following proposition.   Let a topological space and let be a subset of . Following topologies defined on are all equivalent.    The topology .    The coarsest topology on for which the canonical inclusion is continuous.    (Universal property of subspace topology). The topology on satisfying the following property.  For every topological space and any function , is continuous if and only if is continuous. See the following diagram.   The universal property of the subspace topology           The equivalence of topologies and is left as an exercise.  We show that . Consider the following diagram, where is the identity map.      Note that , and with the topology the inclusion is continuous. By the universal property of , the identity is also continuous. Hence, .  We now consider the following.      As is continuous, using the universal property of , we get that is also continuous. Since the is the coarsest topology that makes inclusion continuous, we have that .     Let be a topological space, and let be a subset of . The subspace topology on is any of the topologies defined by .    Suppose that . We can consider a product topology on as well as subspace topology induced by a product topology on . Are these topologies same on ?  (Group of invertible matrices as a topological space)   We consider as a subset of , and we give subspace topology on . As the entries of product of matrices depend continuously on entries of and , the multiplication map is continuous. Furthermore, the entries of the inverse of depends continuously on the entries of . Hence, the inverse map given by is continuous.    "
 },
 {
   "id": "prop-subspace-top",
@@ -215,6 +215,15 @@ var ptx_lunr_docs = [
   "number": "1.3.6",
   "title": "",
   "body": "Suppose that . We can consider a product topology on as well as subspace topology induced by a product topology on . Are these topologies same on ? "
+},
+{
+  "id": "GL-as-topological-group",
+  "level": "2",
+  "url": "sec-subspace-top.html#GL-as-topological-group",
+  "type": "Example",
+  "number": "1.3.7",
+  "title": "(Group of invertible matrices as a topological space).",
+  "body": "(Group of invertible matrices as a topological space)   We consider as a subset of , and we give subspace topology on . As the entries of product of matrices depend continuously on entries of and , the multiplication map is continuous. Furthermore, the entries of the inverse of depends continuously on the entries of . Hence, the inverse map given by is continuous.   "
 },
 {
   "id": "backmatter-2",
